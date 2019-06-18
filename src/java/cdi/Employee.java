e484472392607ab0dea49c21ed5a4c1df6b41ef9
akira shimada <shimada.ak@gmail.com>
2019-06-18 22:29:32 +0900

    Employee.java ・@RequestScopedを@ViewScopedへ変更 ・CDIビーンのプロパティのうち、TEmployeeと重複するものを削除し、代わりにTEmployeeをプロパティとして追加 ・showMessage()はテスト用に一時的に作成したメソッドなので削除
    EmployeeList.java
    ・不要なインポートとコメントを削除
    
    web.xml
    ・コンテキストルートのページを変更
    
    employeeDetail.xhtml
    ・バリデーションを追加

--- src/java/cdi/Employee.java
+++ src/java/cdi/Employee.java
@@ -6,21 +6,14 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-/**
- *
- * @author owner
- */
 @Named
-@RequestScoped
+@ViewScoped
 public class Employee implements Serializable {
     public enum Mode {
         Undefined,
@@ -29,13 +22,7 @@ public class Employee implements Serializable {
         Edit
     }
     private String employeeId;
-    private String name;
-    private String gender;
-    private String phone;
-    private String mobilePhone;
-    private String zipCode;
-    private String address;
-    private String remarks;
+    private TEmployee entity;
     private Mode mode = Mode.Undefined;
     
     @PersistenceContext
@@ -60,20 +47,20 @@ public class Employee implements Serializable {
         
         System.out.println(">>> Employee initQueryParameters() BEGIN >>>");
         if(employeeId != null && !employeeId.equals("")) {
-            TEmployee tEmployee = em.find(TEmployee.class, employeeId);
-            if(tEmployee != null) {
-                System.out.println("employee found! [employeeId=" + tEmployee.getEmployee_id() + ", mode=" + mode.name() + "]");
-                employeeId = tEmployee.getEmployee_id();
-                name = tEmployee.getName();
-                gender = tEmployee.getGender();
-                phone = tEmployee.getPhone();
-                mobilePhone = tEmployee.getMobilePhone();
-                zipCode = tEmployee.getZipCode();
-                address = tEmployee.getAddress();
-                remarks = tEmployee.getRemarks();
+            entity = em.find(TEmployee.class, employeeId);
+            if(entity != null) {
+                System.out.println("employee found! [employeeId=" + entity.getEmployee_id() + ", mode=" + mode.name() + "]");
             }
         }
         System.out.println("<<< Employee initQueryParameters() END <<<");
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
     }
     
     private String getViewId() throws UnsupportedEncodingException
@@ -84,71 +71,11 @@ public class Employee implements Serializable {
         System.out.println("viewId=" + viewId);
         return viewId;
     }
+
+    public TEmployee getEntity() {
+        return entity;
+    }
     
-    public String getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(String employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getMobilePhone() {
-        return mobilePhone;
-    }
-
-    public void setMobilePhone(String mobilePhone) {
-        this.mobilePhone = mobilePhone;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getRemarks() {
-        return remarks;
-    }
-
-    public void setRemarks(String remarks) {
-        this.remarks = remarks;
-    }
-
     public String getMode() {
         return mode.name();
     }
@@ -185,43 +112,11 @@ public class Employee implements Serializable {
     public void save()
     {
         System.out.println(">>> Employee save() BEGIN >>>");
-        TEmployee tEmployee = em.find(TEmployee.class, employeeId);
-        if(tEmployee==null) {
-            tEmployee = new TEmployee();
-            tEmployee.setAddress(address);
-            tEmployee.setEmployee_id(employeeId);
-            tEmployee.setGender(gender);
-            tEmployee.setMobilePhone(mobilePhone);
-            tEmployee.setName(name);
-            tEmployee.setPhone(phone);
-            tEmployee.setRemarks(remarks);
-            tEmployee.setZipCode(zipCode);
-            em.persist(tEmployee);
+        if(entity==null) {
+            em.persist(entity);
         } else {
-            tEmployee.setAddress(address);
-            tEmployee.setEmployee_id(employeeId);
-            tEmployee.setGender(gender);
-            tEmployee.setMobilePhone(mobilePhone);
-            tEmployee.setName(name);
-            tEmployee.setPhone(phone);
-            tEmployee.setRemarks(remarks);
-            tEmployee.setZipCode(zipCode);
-            em.merge(tEmployee);
+            em.merge(entity);
         }
         System.out.println("<<< Employee save() END <<<");
    }
-
-    public void showMessage()
-    {
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        String message =  "名前:%s\n "
-                        + "性別:%s\n "
-                        + "電話番号:%s\n "
-                        + "携帯電話番号:%s \n "
-                        + "郵便番号:%s \n"
-                        + "住所:%s \n";
-        message = String.format(message, name, gender, phone, mobilePhone, zipCode, address);
-        FacesMessage normalMessage = new FacesMessage(message);
-        facesContext.addMessage(null, normalMessage);
-    }
 }
--- src/java/cdi/EmployeeList.java
+++ src/java/cdi/EmployeeList.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cdi;
 
-import cdi.Employee.Mode;
 import entity.TEmployee;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -13,8 +7,6 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.context.FacesContext;
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -62,34 +54,5 @@ public class EmployeeList {
         
         System.out.println(String.format("<<< EmployeeList gotoDetail(%s,%s) END <<<", employeeId, mode));
         return "employeeDetail?faces-redirect=true&employee_id=" + URLEncoder.encode(employeeId, "UTF-8") + "&mode=" + URLEncoder.encode(mode, "UTF-8");
-        
-        /*
-        FlashオブジェクトでTEmployeeをまるまる遷移先に渡す。
-        ⇒遷移先画面でブラウザの更新ボタンを押すと、Flashオブジェクトから
-          設定が消え、入力項目が空欄になる。
-          ViewScopedはブラウザの更新を行ってもViewIdが変わらなければ
-          CDIビーンは保持されると想定していたが、
-          実際には他画面の遷移時と同様、CDIビーンは破棄されてしまう。
-        
-        FacesContext.getCurrentInstance()
-                    .getExternalContext()
-                    .getFlash()
-                    .put("employee", tEmployee);
-        return "employeeDetail?faces-redirect=true";
-        */
-        /*
-            下記だとリダイレクトでHTTPリクエストが2回発生するので
-            設定した値が初期化されてしまう。
-        
-        employee.setAddress(tEmployee.getAddress());
-        employee.setEmployeeId(tEmployee.getEmployee_id());
-        employee.setGender(tEmployee.getGender());
-        employee.setMobilePhone(tEmployee.getMobilePhone());
-        employee.setName(tEmployee.getName());
-        employee.setPhone(tEmployee.getPhone());
-        employee.setRemarks(tEmployee.getRemarks());
-        employee.setZipCode(tEmployee.getZipCode());
-        return "employeeDetail?faces-redirect=true";  //return "employeeDetail";
-        */
     }
 }
