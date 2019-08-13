247dbb1b5f6f6f871de2ed5385910213d86dd932
akira shimada <shimada.ak@gmail.com>
2019-08-14 01:31:37 +0900

    JSFにて例外が発生した場合、errorException.xhtmlへ遷移する機能を追加

---
+++ src/java/jsf/exception/CustomExceptionHandlerFactory.java
@@ -0,0 +1,21 @@
+package jsf.exception;
+
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerFactory;
+
+/**
+ *
+ * @author Owner
+ */
+public class CustomExceptionHandlerFactory extends ExceptionHandlerFactory {
+    private final ExceptionHandlerFactory parent;
+
+    public CustomExceptionHandlerFactory(ExceptionHandlerFactory parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public ExceptionHandler getExceptionHandler() {
+        return new CustomExceptionHandler(parent.getExceptionHandler());
+    }
+}
