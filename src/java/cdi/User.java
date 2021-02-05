bef546baa5d5b4d8ceaf66d44fb1a4604d361e51
akiratto <shimada.ak@gmail.com>
2021-02-06 00:31:16 +0900

    MavenからAntに戻す。ログイン機能を追加。

---
+++ src/java/cdi/User.java
@@ -0,0 +1,69 @@
+package cdi;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.print.attribute.standard.Severity;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Owner
+ */
+@Named
+@RequestScoped
+public class User {
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String login()
+    {
+        HttpServletRequest request 
+                = (HttpServletRequest)FacesContext.getCurrentInstance()
+                                                  .getExternalContext()
+                                                  .getRequest();
+        try {
+            request.login(userName, password);
+        } catch(Exception ex) {
+            FacesContext.getCurrentInstance()
+                        .addMessage("", new FacesMessage(FacesMessage.SEVERITY_ERROR, 
+                                                         "ユーザ名またはパスワードに誤りがあります。", ""));
+            ex.printStackTrace();
+        }
+        return "employeeList.xhtml?faces-redirect=true";
+    }
+    
+    public String logout()
+    {
+        //セッションを終了する
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        HttpServletRequest request 
+                = (HttpServletRequest)FacesContext.getCurrentInstance()
+                                                  .getExternalContext()
+                                                  .getRequest();
+        try {
+            request.logout();
+        } catch(ServletException ex) {
+            ex.printStackTrace();
+        }
+        return "index.xhtml?faces-redirect=true";
+    }
+}
